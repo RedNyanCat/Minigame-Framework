461d33fcda4b9fb7228e81e44eba55021312188e
RedNyanCat <awsomenessAlex20@gmail.com>
2023-05-24 04:48:30 +0800

    Initial Push
    Blank Plugin Template in Kotlin

---
+++ Minigame Framework/src/main/java/com/ecues/MinigameFramework.java
@@ -0,0 +1,17 @@
+package com.ecues;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinigameFramework extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
